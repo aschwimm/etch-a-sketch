4fdef5f914b29d3a27816d30821e0fc093420576
Alex <schwimmeralexander@gmail.com>
2024-02-25 23:36:07 -0700

    Add local files into repo

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function drawGrid(gridRows, gridColumns) {
+    const container = document.querySelector(".container");
+    for(let i = 0; i < gridRows; i++) {
+        const subContainer = document.createElement("div");
+        subContainer.classList.add("sub-container")
+        for(let j = 0; j < gridColumns; j++) {
+            const gridElement = document.createElement("div");
+            gridElement.classList.add("grid-element");
+            subContainer.appendChild(gridElement);
+        }
+        container.appendChild(subContainer);
+    }
+}
